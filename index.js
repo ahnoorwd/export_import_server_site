3bac4f5c6eac176d9f800944b4c7406bd7f3f8fc
ahmed nur <ahmednur103308@gmail.com>
2025-11-23 13:52:30 +0600

    Complete the project with mongodb data base

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+   // await client.connect();
 
     const db = client.db("products_db");
     const productcollection = db.collection("products");
@@ -78,11 +78,11 @@ async function run() {
       const product = await productcollection.findOne({
         _id: new ObjectId(productId),
       });
-
+      console.log(product);
       if (!product) {
         return res.status(404).send({ message: "Product not found" });
       }
-
+    
       // Save import record
       const importData = {
         userEmail, // ⭐ store the logged-in user's email
@@ -211,7 +211,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    //await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
